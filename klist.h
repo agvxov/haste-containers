98fc0fe09aa0829f4d2cafa15ff2d7de0367ec8f
anon <anon@anon.anon>
2025-10-10 18:49:06 +0200

    remove type argument from kvec

--- klist.h
+++ klist.h
@@ -3,79 +3,13 @@
 
 #include <kvec.h>
 
-/* NOTE: C++ is shitware for the mentally handicapped.
- *        For this reason, it removes a bunch of useful, harmless,
- *         exciting, and holy edgecases which were put there by God himself.
- *        As a result, we have to hack around it.
- */
-
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdint.h>
 
 #define klnode_t(type) struct { type d; size_t p, n; }
-#define klist_t(type) struct { size_t h, t; size_t n; kvec_t(klnode_t(type)) a; }
-/* C++ Problem 1)
- * It disallows conversions from void*, but keeps the no anonymous type folding
- *  within the same compile unit rule from C.
- * It also conveniently forgets to counter-act this with some decltype hack,
- *  e.g. this is illegal:
- *
- *      struct { int i; } a;
- *
- *      decltype( ([] constexpr { struct { int i; } a; return a; })() )* b
- *      = (decltype( ([] constexpr { struct { int i; } a; return a; })() )*)&a;
- *
- * Not to mention that the syntax has been buggy in G++ for years, but I digress.
- *
- * As a result, the original iterator:
- *      #define kliter_t(type) klnode_t(type)*
- * Proved to be unfeasible.
- * One partial solution would have been to use auto,
- *  but then it would be impossible to have uninitialized iterators,
- *  e.g. for some selection task.
- *
- * This, btw is why we depend on C23. Because either typeof,
- *  or further macro mangling to cast / not cast the iterator function return values.
- */
-#ifdef __cplusplus
-    #define kliter_t(l) decltype(l.a.a)
-#else
-    #define kliter_t(l) typeof(l.a.a)
-#endif
-
-/* C++ Problem 2)
- *  > error: types may not be defined in ‘sizeof’ expressions
- *  Truly genius, I bet Bjorn Stonesoup lies awake at night,
- *   adoring himself for inventing this.
- *  Anyways, we can fix this by lobotomy.
- */
-#ifdef __cplusplus
-    #define LOBOTOMIZED_TYPE_DEFINITION_SUBVERTED_INSIDE_SIZEOF(...) \
-     ([] constexpr { __VA_ARGS__ i; return sizeof(i); })()
-#else
-    #define LOBOTOMIZED_TYPE_DEFINITION_SUBVERTED_INSIDE_SIZEOF(...) sizeof(__VA_ARGS__)
-#endif
-
-/* C++ Problem 3)
- * > error: invalid conversion from ‘void*’ to ‘[...]<unnamed struct>*’
- * Oh Severin!
- * At this time around the interface wont have to change...
- */
-#ifdef __cplusplus
-    #define MY_TYPE_CAST(v) (decltype((v).a))
-#else
-    #define MY_TYPE_CAST(v)
-#endif
-
-/* Copy of kvec's kv_pushp() with more casts.
- * A modified version is required for type safety in C,
- *  C++ just so happens to further mangle it.
- */
-#define kv_pushp_(type, v) (((v).n == (v).m)?                           \
-                           ((v).m = ((v).m? (v).m<<1 : 2),              \
-                            (v).a = MY_TYPE_CAST(v) realloc((v).a, (LOBOTOMIZED_TYPE_DEFINITION_SUBVERTED_INSIDE_SIZEOF(type)) * (v).m), 0) \
-                           : 0), ((v).a + ((v).n++))
+#define klist_t(type)  struct { size_t h, t; size_t n; kvec_t(klnode_t(type)) a; }
+#define kliter_t(l) typeof(l.a.a)
 
 #define kl_init(l) (l.h = l.t = SIZE_MAX, l.n = 0, kv_init(l.a))
 #define kl_destroy(l) kv_destroy(l.a)
@@ -86,8 +20,8 @@
 #define kl_size(l)     l.n
 #define kl_d(i)        i->d
 
-#define kl_add(type, l, x) ( \
-    kv_pushp_(klnode_t(type), l.a), \
+#define kl_add(l, x) (\
+    kv_pushp(l.a), \
     kv_A(l.a, kv_size(l.a)-1).d = x, \
     (l.t != SIZE_MAX \
         ? ( \
--- kvec.h
+++ kvec.h
@@ -1,34 +1,30 @@
-/* The MIT License
- * Copyright (c) 2008, by Attractive Chaos <attractor@live.co.uk> Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
+#ifndef AC_KVEC_H
+#define AC_KVEC_H
 /*
   An example:
 
-#include "kvec.h"
-int main() {
-	kvec_t(int) array;
-	kv_init(array);
-	kv_push(int, array, 10); // append
-	kv_a(int, array, 20) = 5; // dynamic
-	kv_A(array, 20) = 4; // static
-	kv_destroy(array);
-	return 0;
-}
+    #include "kvec.h"
+    int main() {
+        kvec_t(int) array;
+        kv_init(array);
+        kv_push(int, array, 10);  // append
+        kv_a(int, array, 20) = 5; // dynamic
+        kv_A(array, 20) = 4;      // static
+        kv_destroy(array);
+        return 0;
+    }
 */
-
-/*
-  2008-09-22 (0.1.0):
-
-	* The initial version.
-
-*/
-
-#ifndef AC_KVEC_H
-#define AC_KVEC_H
+#if __STDC_VERSION__ >= 202311L
+#elif __cplusplus >= 202302L
+#else
+# error "C/C++23 required for kvec."
+#endif
+#ifdef __cplusplus
+# define typeof decltype
+#endif
 
 #include <stdlib.h>
-extern void * memcpy(void *, const void *, size_t);
+#include <string.h>
 
 #define kv_roundup32(x) (--(x), (x)|=(x)>>1, (x)|=(x)>>2, (x)|=(x)>>4, (x)|=(x)>>8, (x)|=(x)>>16, ++(x))
 
@@ -40,38 +36,55 @@ extern void * memcpy(void *, const void *, size_t);
 #define kv_size(v)    ((v).n)
 #define kv_max(v)     ((v).m)
 
-#define kv_resize(type, v, s) ((v).m = (s), (v).a = (type*)realloc((v).a, sizeof(type) * (v).m))
+#define kv_resize(v, s) (                                               \
+    (v).m = (s),                                                        \
+    (v).a = (typeof(v.a))realloc((v).a, sizeof(typeof(v.a[0])) * (v).m) \
+)
 
-#define kv_copy(type, vd, vs) do {							\
-		if ((vd).m < (vs).n) kv_resize(type, vd, (vs).n);	\
-		(vd).n = (vs).n;									\
-		memcpy((vd).a, (vs).a, sizeof(type) * (vs).n);		\
-	} while (0)												\
-
-
-#define kv_append(type, v1, v0) do {                                    \
-    if ((v1).m < (v1).n + (v0).n) kv_resize(type, v1, (v1).n + (v0).n); \
-    memcpy((v1).a + (v1).n, (v0).a, sizeof(type) * (v0).n);             \
-    (v1).n += (v0).n;                                                   \
+#define kv_copy(vd, vs) do {                                  \
+    if ((vd).m < (vs).n) kv_resize(vd, (vs).n);               \
+    (vd).n = (vs).n;                                          \
+    memcpy((vd).a, (vs).a, sizeof(typeof(vs.a[0])) * (vs).n); \
 } while (0)
 
-#define kv_push(type, v, x) do {									\
-		if ((v).n == (v).m) {										\
-			(v).m = (v).m? (v).m<<1 : 2;							\
-			(v).a = (type*)realloc((v).a, sizeof(type) * (v).m);	\
-		}															\
-		(v).a[(v).n++] = (x);										\
-	} while (0)
 
-#define kv_pushp(type, v) (((v).n == (v).m)?							\
-						   ((v).m = ((v).m? (v).m<<1 : 2),				\
-							(v).a = (type*)realloc((v).a, sizeof(type) * (v).m), 0)	\
-						   : 0), ((v).a + ((v).n++))
+#define kv_append(v1, v0) do {                                                     \
+    if ((v1).m < (v1).n + (v0).n) kv_resize(typeof(v1.a[0]), v1, (v1).n + (v0).n); \
+    memcpy((v1).a + (v1).n, (v0).a, sizeof(typeof(v0.a[0]) * (v0).n);              \
+    (v1).n += (v0).n;                                                              \
+} while (0)
 
-#define kv_a(type, v, i) (((v).m <= (size_t)(i)? \
-						  ((v).m = (v).n = (i) + 1, kv_roundup32((v).m), \
-						   (v).a = (type*)realloc((v).a, sizeof(type) * (v).m), 0) \
-						  : (v).n <= (size_t)(i)? (v).n = (i) + 1 \
-						  : 0), (v).a[(i)])
+#define kv_push(v, x) do {                                                   \
+    if ((v).n == (v).m) {                                                    \
+        (v).m = (v).m ? (v).m<<1 : 2;                                        \
+        (v).a = (typeof(v.a))realloc((v).a, sizeof(typeof(v.a[0])) * (v).m); \
+    }                                                                        \
+    (v).a[(v).n++] = (x);                                                    \
+} while (0)
+
+#define kv_pushp(v) (                                                                      \
+    (                                                                                      \
+        ((v).n == (v).m) ?                                                                 \
+            (                                                                              \
+                (v).m = ((v).m ? (v).m<<1 : 2),                                            \
+                (v).a = (typeof((v).a))realloc((v).a, sizeof(typeof((v).a[0])) * (v).m), 0 \
+            )                                                                              \
+        :                                                                                  \
+            0                                                                              \
+    ),                                                                                     \
+    (v).a + (v).n++                                                                        \
+)
+
+#define kv_a(v, i) (                                                                           \
+    (                                                                                      \
+        (v).m <= (size_t)(i) ?                                                             \
+            (                                                                              \
+                (v).m = (v).n = (i) + 1, kv_roundup32((v).m),                              \
+                (v).a = (typeof((v).a))realloc((v).a, sizeof(typeof((v).a[0])) * (v).m), 0 \
+            )                                                                              \
+        : (v).n <= (size_t)(i) ? (v).n = (i) + 1 : 0                                       \
+    ),                                                                                     \
+    (v).a[(i)]                                                                             \
+)
 
 #endif
