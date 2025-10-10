276b41cd0d88f0ea4fea6db5c3c2e923afe0e728
anon <anon@anon.anon>
2025-10-10 18:48:27 +0200

    ring buffer from barrel in a wig

--- kringbuffer.h
+++ kringbuffer.h
@@ -1,15 +1,15 @@
 #ifndef KRINGBUFFER_H
 #define KRINGBUFFER_H
 
-#define kringbuffer_t(type, max) struct { type a[max]; size_t cur; }
-#define krb_init(b)     ((b).cur = 0)
-#define krb_cap(b)      (sizeof((b).a) / sizeof((b).a[0]))
-#define krb_shr(b)      (((b).cur == krb_cap(b)) ? ((b).et = 0)            : ((b).et += 1))
-#define krb_shl(b)      (((b).cur == 0)          ? ((b).et = krb_cap(b)-1) : ((b).et -= 1))
-#define krb_top(b)      ((b).a[(b).cur])
-#define krb_push(b, i) ( \
-    krb_top(b) = i,      \
-    krb_shr(),           \
+#define kringbuffer_t(type, max) struct { type a[max]; size_t h; }
+#define krb_init(b)     ((b).h = 0)
+#define krb_size(b)     (sizeof((b).a) / sizeof((b).a[0]))
+#define krb_A(b, i)     ((b).a[i])
+#define krb_a(b, i)     ((b).a[(i) % krb_size(b)])
+#define krb_top(b)      krb_a(b, b.h + krb_size(b) - 1)
+#define krb_push(b, i) (               \
+    (b).a[(b).h] = i,                  \
+    (b).h = ((b).h + 1) % krb_size(b)  \
 )
 
 #endif
