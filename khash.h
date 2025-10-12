56b072d39d622c817d2a8f054d6c7882ad802096
anon <anon@anon.anon>
2025-10-12 16:22:52 +0200

    khash PROTOTYPE

---
+++ khash.h
@@ -0,0 +1,362 @@
+#ifndef __AC_KHASHL_H
+#define __AC_KHASHL_H
+
+/* TODO:
+ *  - proper set interface; making sure the two dont mix
+ *  - *_core could be called directly, probably(?)
+ *  - strings segvs
+ */
+
+#if __STDC_VERSION__ >= 202311L
+#elif __cplusplus >= 202302L
+#else
+# error "C/C++23 required for khash."
+#endif
+#ifdef __cplusplus
+# define typeof decltype
+# define register
+#endif
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+typedef struct {
+    int32_t bits;
+    int32_t size;
+    int32_t * used;
+    // map -> (key, value)[]
+    // set -> key[]
+    char * payloads;
+} khmeta_t;
+
+/***************************
+ * Must be defined up high *
+ ***************************/
+#pragma region
+#define SCOPE [[maybe_unused]] static
+
+#ifndef kh_max_load_factor
+# define kh_max_load_factor(cap) (((cap)>>1) + ((cap)>>2)) /* 75% */
+#endif
+
+#define _type_check_key(h, k) 1 ? (typeof(k+0)*)k : &h.type_guard->key_type_guard
+#define _type_check_val(h, v) 1 ? (typeof(v+0)*)v : &h.type_guard->value_type_guard
+
+typedef int32_t (*hash_fn_t)(...);
+typedef bool    (*bool_fn_t)(...);
+
+static inline int32_t fmix32(int32_t x);
+static inline int32_t issue19(int32_t x);
+static inline int32_t splitmix64(int64_t x);
+static inline int32_t FNV1a32(const char * s);
+
+#define kh_hash_uint32 fmix32
+#define kh_hash_uint64 splitmix64
+#define kh_hash_str    FNV1a32
+
+#define kh_eq_intN(N)\
+    static inline bool kh_eq_int##N (void * a, int##N##_t b) { return *(int##N##_t*)a  == b; }
+kh_eq_intN(8) kh_eq_intN(16) kh_eq_intN(32) kh_eq_intN(64)
+static inline bool kh_eq_str(char * a, char * b) { return !strcmp(a, b); }
+
+#ifndef __cplusplus
+    #define hash_generic(x) (hash_fn_t)_Generic(x \
+        , int32_t       : kh_hash_uint32 \
+        , int64_t       : kh_hash_uint64 \
+        , char *        : kh_hash_str \
+        , const char *  : kh_hash_str \
+    )
+    #define eq_generic(x) (bool_fn_t)_Generic(x \
+        , int8_t        : kh_eq_int8  \
+        , int16_t       : kh_eq_int16 \
+        , int32_t       : kh_eq_int32 \
+        , int64_t       : kh_eq_int64 \
+        , char *        : kh_eq_str   \
+        , const char *  : kh_eq_str   \
+    )
+#else
+    hash_fn_t hash_generic(int32_t i) { return (hash_fn_t)kh_hash_uint32; }
+    hash_fn_t hash_generic(int64_t i) { return (hash_fn_t)kh_hash_uint64; }
+    hash_fn_t hash_generic(char * i)  { return (hash_fn_t)kh_hash_str; }
+    #define eq_genericN(N) bool_fn_t eq_generic(int##N##_t i) { return (bool_fn_t)kh_eq_int##N; }
+    eq_genericN(8) eq_genericN(16) eq_genericN(32) eq_genericN(64)
+    bool_fn_t eq_generic(char * i)    { return (bool_fn_t)kh_eq_str; }
+#endif
+
+static inline bool __kh_is_used(int32_t * flag, int32_t i) { return (flag[i>>5] >> (i&0x1fU) & 1U); }
+static inline void __kh_set_used(int32_t * flag, int32_t i) { flag[i>>5] |= 1U<<(i&0x1fU); }
+static inline void __kh_set_unused(int32_t * flag, int32_t i) { flag[i>>5] &= ~(1U<<(i&0x1fU)); }
+
+#define __kh_sizeof_used(n_buckets) ((n_buckets) < 32 ? 1 : (n_buckets)>>5)
+#define __kh_get_n_buckets(h) ((int32_t)1U << (h)->bits)
+#pragma endregion
+
+/**************************
+ * Public macro functions *
+ **************************/
+#pragma region
+#define khash(key_type, value_type)      \
+    union {                              \
+        khmeta_t meta;                   \
+        struct {                         \
+            key_type key_type_guard;     \
+            value_type value_type_guard; \
+        } * type_guard;                  \
+    }
+#define khiter_t(h) int32_t
+
+#define kh_init(h)    h.meta = (khmeta_t) { .bits = 0, .size = 0, .used = nullptr, .payloads = nullptr }
+#define kh_clear(h)   _kh_clear(&h.meta)
+#define kh_destroy(h) _kh_destroy(&h.meta)
+
+#define kh_size(h)     ((h).meta->size)
+#define kh_capacity(h) ((h).meta.payloads ? 1U<<(h).meta.bits : 0U)
+#define kh_begin(h)    0
+#define kh_end(h)      (int32_t)kh_capacity(h)
+
+#define kh_key(h, x) (( (typeof((h).type_guard))((h).meta.payloads) )[x].key_type_guard)
+#define kh_val(h, x) (( (typeof((h).type_guard))((h).meta.payloads) )[x].value_type_guard)
+#define kh_A(h, k)   ( (typeof((h).type_guard))A(&h.meta, _type_check_key(h, k), sizeof(h.type_guard), hash_generic(k), eq_generic(k)) )->value_type_guard
+
+#define kh_get(h, k)        _kh_get(&h.meta, _type_check_key(h, k), sizeof(h.type_guard), hash_generic(k), eq_generic(k))
+#define kh_ensure_key(h, v) _kh_ensure_key(&h.meta, _type_check_val(h, v), sizeof(h.type_guard), hash_generic(k), eq_generic(k))
+#define kh_del(h, it)       _kh_del(&h.meta, sizeof(h.type_guard), it)
+#define kh_resize(h, n)     _kh_resize(h, sizeof(h.type_guard), new_n_buckets, hash_generic(k))
+#define kh_exists(h, k)     (kh_get(h, k) != kh_end(h)) // XXX buggy
+
+#define kh_foreach(it, h) \
+    for (int _i = 0, _flag = 1; _flag && _i != kh_end(h); ++_i, _flag = !_flag) \
+        for (it = _i; _flag; _flag = !_flag) \
+            if (__kh_is_used((h).meta.used, _i))
+#pragma endregion
+
+/*************************
+ * Common hash functions *
+ *************************/
+#pragma region
+static inline int32_t __kh_h2b(int32_t hash, int32_t bits) { return hash * 2654435769U >> (32 - bits); } /* Fibonacci hashing */
+
+/* Source: https://github.com/aappleby/smhasher/blob/master/src/MurmurHash3.cpp */
+static inline
+int32_t fmix32(int32_t x) {
+	x ^= x >> 16;
+	x *= 0x85ebca6bU;
+	x ^= x >> 13;
+	x *= 0xc2b2ae35U;
+	x ^= x >> 16;
+	return x;
+}
+
+/* Source: https://github.com/skeeto/hash-prospector/issues/19 */
+static inline
+int32_t issue19(int32_t x) {
+	x ^= x >> 16;
+	x *= 0x21f0aaad;
+	x ^= x >> 15;
+	x *= 0xd35a2d97;
+	x ^= x >> 15;
+	return x;
+}
+
+/* Source: https://xoshiro.di.unimi.it/splitmix64.c */
+static inline
+int32_t splitmix64(int64_t x) {
+	x ^= x >> 30;
+	x *= 0xbf58476d1ce4e5b9ULL;
+	x ^= x >> 27;
+	x *= 0x94d049bb133111ebULL;
+	x ^= x >> 31;
+	return (int32_t)x;
+}
+
+// XXX: try cityhash (https://github.com/google/cityhash/blob/master/src/city.cc)
+static inline
+int32_t FNV1a32(const char * s) {
+	int32_t h = 2166136261U;
+	const unsigned char * t = (const unsigned char*)s;
+	for (; *t; ++t) {
+		h ^= *t;
+        h *= 16777619;
+    }
+	return h;
+}
+#pragma endregion
+
+/****************************
+ * Implementation internals *
+ ****************************/
+#pragma region
+SCOPE
+void _kh_destroy(khmeta_t * meta) {
+    free(meta->used);
+    free(meta->payloads);
+}
+
+SCOPE
+void _kh_clear(khmeta_t * h) {
+    if (h && h->used) {
+        int32_t n_buckets = __kh_get_n_buckets(h);
+        memset(h->used, 0, __kh_sizeof_used(n_buckets) * sizeof(int32_t));
+        h->size = 0;
+    }
+}
+
+SCOPE
+int _kh_resize(khmeta_t *h, int payload_size, int32_t new_n_buckets, hash_fn_t hash_fn) {
+	int32_t * new_used = nullptr;
+    int32_t new_bits = 0;
+
+	while ((new_n_buckets >>= 1) != 0) { ++new_bits; }
+	if (new_n_buckets & (new_n_buckets - 1)) { ++new_bits; }
+	new_bits = new_bits > 2 ? new_bits : 2;
+	new_n_buckets = (int32_t)1U << new_bits;
+
+    // requested size is too small
+	if (h->size > kh_max_load_factor(new_n_buckets)) { return 0; }
+
+	new_used = (typeof(new_used))malloc(sizeof(int32_t) * __kh_sizeof_used(new_n_buckets));
+	if (!new_used) { return -1; }
+
+	memset(new_used, 0, __kh_sizeof_used(new_n_buckets) * sizeof(int32_t));
+	int32_t n_buckets = h->payloads ? __kh_get_n_buckets(h) : 0U;
+	if (n_buckets < new_n_buckets) { /* expand */
+		char * new_keys = (typeof(new_keys))realloc(h->payloads, payload_size * new_n_buckets);
+		if (!new_keys) {
+            free(new_used);
+            return -1;
+        }
+		h->payloads = new_keys;
+	}
+	for (int32_t j = 0; j != n_buckets; ++j) {
+		if (!__kh_is_used(h->used, j)) { continue; }
+
+		char * key = h->payloads + payload_size*j;
+		__kh_set_unused(h->used, j);
+        // Kick-out process (sort of like in Cuckoo hashing)
+		for (int32_t i; true;) {
+			i = __kh_h2b(hash_fn(key), new_bits);
+	        int32_t new_mask = new_n_buckets - 1;
+			while (__kh_is_used(new_used, i)) { i = (i + 1) & new_mask; }
+			__kh_set_used(new_used, i);
+            // kick out the existing element
+			if (i < n_buckets && __kh_is_used(h->used, i)) {
+				{
+                    char tmp[payload_size];
+                    memcpy(tmp, h->payloads + payload_size*i, payload_size);
+                    memcpy(h->payloads,  key, payload_size);
+                    memcpy(key,  tmp, payload_size);
+                    key = tmp;
+                }
+				__kh_set_unused(h->used, i);
+			} else {
+                memcpy(h->payloads + payload_size*i, key, payload_size);
+				break;
+			}
+		}
+	}
+
+    // Shrink
+	if (n_buckets > new_n_buckets) {
+		char * new_keys = (typeof(new_keys))realloc(h->payloads, payload_size * new_n_buckets);
+		if (!new_keys) {
+            free(new_used);
+            return -1;
+        }
+		h->payloads = new_keys;
+	}
+
+	free(h->used);
+	h->used = new_used;
+    h->bits = new_bits;
+
+	return 0;
+}
+
+SCOPE
+int _kh_del(khmeta_t *h, int payload_size, int32_t i, hash_fn_t hash_fn) {
+	if (h->payloads == nullptr) { return 0; }
+
+	int32_t j = i;
+	int32_t n_buckets = __kh_get_n_buckets(h);
+	int32_t mask = n_buckets - 1U;
+	while (true) {
+		j = (j + 1U) & mask;
+        /* j==i only when the table is completely full */
+		if (j == i || !__kh_is_used(h->used, j)) { break; }
+		int32_t k = __kh_h2b(hash_fn(h->payloads + j*payload_size), h->bits);
+		if ((j > i && (k <= i || k > j)) || (j < i && (k <= i && k > j))) {
+            memcpy(h->payloads + i*payload_size, h->payloads + j*payload_size, payload_size);
+			i = j;
+        }
+	}
+	__kh_set_unused(h->used, i);
+	--h->size;
+
+	return 1;
+}
+
+SCOPE
+int32_t _kh_get_core(const khmeta_t * h, const void * key, int payload_size, int32_t hash, bool_fn_t eq) {
+	if (h->payloads == nullptr) { return 0; }
+
+	int32_t n_buckets = __kh_get_n_buckets(h);
+	int32_t mask = n_buckets - 1U;
+	int32_t last, i = last = __kh_h2b(hash, h->bits);
+	while (__kh_is_used(h->used, i) && !eq(h->payloads + i*payload_size, key, payload_size)) {
+		i = (i + 1U) & mask;
+		if (i == last) { return n_buckets; }
+	}
+
+	return !__kh_is_used(h->used, i) ? n_buckets : i;
+}
+
+SCOPE
+int32_t _kh_get(const khmeta_t *h, const void * key, int payload_size, hash_fn_t hash_fn, bool_fn_t eq) {
+    return _kh_get_core(h, key, payload_size, hash_fn(key), eq);
+}
+
+SCOPE
+int32_t _kh_ensure_key_core(khmeta_t *h, const void * key, int payload_size, int32_t hash, hash_fn_t hash_fn, bool_fn_t eq) {
+	int32_t n_buckets = h->payloads ? __kh_get_n_buckets(h) : 0U;
+
+    // Rehashing
+    // TODO: to implement automatically shrinking; resize() already support shrinking
+	if (h->size >= kh_max_load_factor(n_buckets)) {
+		if (_kh_resize(h, payload_size, n_buckets + 1U, hash_fn) < 0) {
+			return n_buckets;
+        }
+		n_buckets = __kh_get_n_buckets(h);
+	}
+
+    // Look for key
+	int32_t mask = n_buckets - 1U;
+	int32_t last, i = last = __kh_h2b(hash, h->bits);
+	while (__kh_is_used(h->used, i) && !eq(h->payloads + i*payload_size, key, payload_size)) {
+		i = (i + 1U) & mask;
+		if (i == last) { break; }
+	}
+
+    // Insert when not present
+	if (!__kh_is_used(h->used, i)) {
+        memcpy(h->payloads + i*payload_size, key, payload_size);
+		__kh_set_used(h->used, i);
+		++h->size;
+	}
+
+	return i;
+}
+
+SCOPE
+int32_t _kh_ensure_key(khmeta_t *h, const void * key, int payload_size, hash_fn_t hash_fn, bool_fn_t eq) {
+    return _kh_ensure_key_core(h, &key, payload_size, hash_fn(key), hash_fn, eq);
+}
+
+SCOPE
+void * A(khmeta_t * h, const void * key, int payload_size, hash_fn_t hash_fn, bool_fn_t eq) {
+    register int i = _kh_ensure_key(h, key, payload_size, hash_fn, eq);
+    return ((char*)h->payloads) + i*payload_size;
+}
+#pragma endregion
+
+#endif
