41be059b8e04eb8b8281392d9666dbf6ad6058ba
anon <anon@anon.anon>
2025-10-12 16:23:00 +0200

    khash tests

---
+++ test/kh_test-int.c
@@ -0,0 +1,25 @@
+// @BAKE gcc -o $*.out $@ -std=c23 -ggdb
+#include <stdio.h>
+#include "khash.h"
+
+int main(void) {
+    khash(int32_t, int32_t) h;
+    kh_init(h);
+
+    kh_A(h, 10) = 1;
+    kh_A(h, 20) = 2;
+    kh_A(h, 40) = 4;
+
+    if (kh_exists(h, 30)) {
+        printf("found key '30'\n");
+    }
+
+    kh_foreach(auto i, h) {
+        printf("%u : %d, ", kh_key(h, i), kh_val(h, i));
+    }
+
+    puts("");
+
+    kh_destroy(h);
+    return 0;
+}
---
+++ test/kh_test-str.c
@@ -0,0 +1,26 @@
+// @BAKE gcc -o $*.out $@ -std=c23 -ggdb
+#include <stdio.h>
+#include "khash.h"
+
+int main(void) {
+    khash(char *, char *) h;
+    kh_init(h);
+
+    kh_A(h, "10") = "1";
+    kh_A(h, "20") = "2";
+    kh_A(h, "40") = "4";
+
+    if (kh_exists(h, "30")) {
+        printf("found key '30'\n");
+    }
+
+
+    kh_foreach(auto i, h) {
+        printf("%s : %s, ", kh_key(h, i), kh_val(h, i));
+    }
+
+    puts("");
+
+    destroy(h);
+    return 0;
+}
